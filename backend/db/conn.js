7ed3f9511fd980a89fa4437f8d4b59db165af681
henrique <you@example.com>
2023-08-17 12:08:50 -0300

    adicionando informações de instalação ao readme

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 //conn.js
 const { Sequelize } = require('sequelize')
                                                         //user e senha diferente
-const sequelize = new Sequelize('radar_da_informacao', 'root', 'sucesso', { //alterar o nome do banco, user e senha.
+const sequelize = new Sequelize('radar_da_informacao', 'root', 'root', { //alterar o nome do banco, user e senha.
     host: 'localhost',
     dialect: 'mysql'
 })
